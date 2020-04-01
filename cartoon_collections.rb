14ba5ab2c1c4e280ac2b52aef0d3a99624854a2b
Colton Kaiser <kaiserc2@seattleu.edu>
2020-04-01 02:39:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,5 +11,7 @@ def list_dwarves(array)
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
-  array.each
+  array.each_with_index do |string|
+    puts |string|
+  end
 end
