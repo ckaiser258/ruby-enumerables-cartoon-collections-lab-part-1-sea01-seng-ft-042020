4288c58e926b76411455052f7e29bdc88ec812d3
Colton Kaiser <kaiserc2@seattleu.edu>
2020-04-01 02:44:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |string|
-    puts array.index + 1
+    puts array.index
     puts string
   end
 end
